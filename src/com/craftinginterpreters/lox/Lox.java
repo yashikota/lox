8956050059c4eba3900c6e98019b5921bf8f1554
yashikota <52403688+yashikota@users.noreply.github.com>
2024-10-08 03:38:47 +0900

    first commit

---
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,16 @@
+package com.craftinginterpreters.lox;
+
+import java.io.IOException;
+
+public class Lox {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+//            runFile(args[0]);
+        } else {
+//            runPrompt();
+        }
+    }
+}
